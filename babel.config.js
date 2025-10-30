d25f82d027d0943526601a363f04cb4a860f5f8b
Flaip <flaip10@gmail.com>
2025-10-30 16:25:03 +0000

    feat: add Babel configuration and update package dependencies for navigation and gesture handling

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            // your other plugins would go here (if any)require.resolve('expo-router/babel'),
+            [
+                'module-resolver',
+                {
+                    root: ['.'],
+                    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+                    alias: {
+                        '@src': './src',
+                        '@screens': './src/screens',
+                        '@components': './src/components',
+                        '@state': './src/state',
+                        '@core': './src/core',
+                        '@hooks': './src/hooks',
+                    },
+                },
+            ],
+            'react-native-reanimated/plugin', // MUST be last
+        ],
+    };
+};
